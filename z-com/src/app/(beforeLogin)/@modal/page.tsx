6670be8dd16bae6040ed1ada2ea279231492621b
koreanstudent <hn12344@naver.com>
2024-01-09 14:23:49 +0900

    패러렐 라우트

---
+++ z-com/src/app/(beforeLogin)/@modal/page.tsx
@@ -0,0 +1,3 @@
+export default function Page() {
+  return "페러랠 모달";
+}
--- z-com/src/app/(beforeLogin)/page.tsx
+++ z-com/src/app/(beforeLogin)/page.tsx
@@ -1,11 +1,11 @@
-import styles from "./page.module.css";
+import styles from "@/app/page.module.css";
 import Link from "next/link";
-import zLogo from "../../public/zlogo.png";
+import zLogo from "../../../public/zlogo.png";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
+    <>
       <div className={styles.left}>
         <Image src={zLogo} alt="logo" />
       </div>
@@ -20,6 +20,6 @@ export default function Home() {
           로그인
         </Link>
       </div>
-    </div>
+    </>
   );
 }
