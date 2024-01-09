6e24dab0251fc73cd7242cebcd453fd633b05245
koreanstudent <hn12344@naver.com>
2024-01-09 16:26:21 +0900

    인터셉팅 라우트

--- z-com/src/app/(beforeLogin)/@modal/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function Page() {
-  return "페러랠 모달";
-}
--- z-com/src/app/(beforeLogin)/layout.tsx
+++ z-com/src/app/(beforeLogin)/layout.tsx
@@ -13,3 +13,6 @@ export default function Layout({ children, modal }: Props) {
     </div>
   );
 }
+
+// 주소가 localhost:3001일 때는 children->page.tsx, modal->@modal/default.tsx
+// 주소가 localhost:3001/i/flow/login 때는 chldren->i/flow/login/page.tsx, modal->@modal/i/flow/login/page.tsx
